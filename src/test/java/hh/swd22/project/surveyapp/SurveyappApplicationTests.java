7679289a141c288e0df66ecc0cb8606b7b8f3d44
Henri Väisänen <henri.vaisane@gmail.com>
2020-03-31 16:08:54 +0300

    initial commit - spring initializer template

---
+++ src/test/java/hh/swd22/project/surveyapp/SurveyappApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd22.project.surveyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
