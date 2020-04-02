c6cd9fd14c15aea2d06cd5a706939c39759ea755
Gosols <teemu.a.kosonen@gmail.com>
2020-04-02 10:40:12 +0300

    Question -luokka aloitettu

--- src/main/java/hh/swd22/project/surveyapp/domain/question.java
+++ src/main/java/hh/swd22/project/surveyapp/domain/question.java
@@ -1,4 +1,45 @@
 package hh.swd22.project.surveyapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long questionID;
+
+    //private Survey survey;
+    private String question;
+
+    public question(long sID, String question) {
+        this.surveyID = sID;
+        this.question = question;
+    }
+
+    public long getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(long questionID) {
+        this.questionID = questionID;
+    }
+
+    public long getSurveyID() {
+        return surveyID;
+    }
+
+    public void setSurveyID(long surveyID) {
+        this.surveyID = surveyID;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 }
