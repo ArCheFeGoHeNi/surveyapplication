4a2cfb06156adcf09000a6ca49d962252bc82aa9
Henri Väisänen <henri.vaisane@gmail.com>
2020-04-07 14:10:41 +0300

    Started QuestionController class

---
+++ src/main/java/hh/swd22/project/surveyapp/webcontroller/QuestionController.java
@@ -0,0 +1,12 @@
+package hh.swd22.project.surveyapp.webcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private Question
+
+}
