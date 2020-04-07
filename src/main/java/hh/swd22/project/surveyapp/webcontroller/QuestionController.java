3ba454e36e23e2e80f57f66fa959c6351b21ff65
Henri Väisänen <henri.vaisane@gmail.com>
2020-04-07 14:44:07 +0300

    Mapped /surveylist endpoint

--- src/main/java/hh/swd22/project/surveyapp/webcontroller/QuestionController.java
+++ src/main/java/hh/swd22/project/surveyapp/webcontroller/QuestionController.java
@@ -1,8 +1,10 @@
 package hh.swd22.project.surveyapp.webcontroller;
 
 import hh.swd22.project.surveyapp.domain.QuestionRepository;
+import hh.swd22.project.surveyapp.domain.SurveyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,10 +14,20 @@ public class QuestionController {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private SurveyRepository surveyRepository;
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index () {
 
         return "index"; //index.html
+
+    }
+
+    @RequestMapping(value = "/surveylist")
+    public String surveyList(Model model) {
+
+        model.addAttribute("surveyList", surveyRepository.findAll());
 
     }
 
