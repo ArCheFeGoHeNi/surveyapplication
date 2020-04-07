124d7173f81fccf2fa4a6f1e44c35d58503dc8c2
Gosols <teemu.a.kosonen@gmail.com>
2020-04-07 20:25:15 +0300

    little bit säätöö with conflicts but it's all good now lol

--- src/main/java/hh/swd22/project/surveyapp/domain/Question.java
+++ src/main/java/hh/swd22/project/surveyapp/domain/Question.java
@@ -1,67 +1,4 @@
-<<<<<<< HEAD
-package hh.swd22.project.surveyapp.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import javax.persistence.*;
-
-@Entity //Entity Class not adding piste this time around
-public class Question {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long questionID;
-
-    @ManyToOne
-    @JsonManagedReference //Critical for stopping endless looping
-    @JoinColumn
-    private Survey survey;
-
-    private String question;
-    private String questiontype;
-
-    public Question() {
-
-    }
-
-    public Question(String question, String questiontype, Survey survey) {
-        this.question = question;
-        this.questiontype = questiontype;
-        this.survey = survey;
-    }
-
-    public Long getQuestionID() {
-        return questionID;
-    }
-
-    public void setQuestionID(Long questionID) {
-        this.questionID = questionID;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getQuestiontype() {
-        return questiontype;
-    }
-
-    public void setQuestiontype(String questiontype) {
-        this.questiontype = questiontype;
-    }
-
-    public Survey getSurvey() {
-        return survey;
-    }
-
-    public void setSurvey(Survey survey) {
-        this.survey = survey;
-    }
-=======
 package hh.swd22.project.surveyapp.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -124,5 +61,5 @@ public class Question {
     public void setSurvey(Survey survey) {
         this.survey = survey;
     }
->>>>>>> a65a3028fe54c51a09f7ee0189a2e29104f05676
+
 }
--- src/main/java/hh/swd22/project/surveyapp/webcontroller/QuestionController.java
+++ src/main/java/hh/swd22/project/surveyapp/webcontroller/QuestionController.java
@@ -1,65 +1,4 @@
-<<<<<<< HEAD
-package hh.swd22.project.surveyapp.webcontroller;
 
-import hh.swd22.project.surveyapp.domain.Question;
-import hh.swd22.project.surveyapp.domain.QuestionRepository;
-import hh.swd22.project.surveyapp.domain.SurveyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-
-@Controller
-public class QuestionController {
-
-    @Autowired
-    private QuestionRepository questionRepository;
-
-    @Autowired
-    private SurveyRepository surveyRepository;
-
-    //Mapping /index endpoint to thymeleaf template index.html
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index() {
-
-        return "index"; //index.html
-
-    }
-
-    //Mapping /surveylist endpoint to thymeleaf template surveylist.html
-    @RequestMapping(value = "/surveylist", method = RequestMethod.GET)
-    public String surveyList(Model model) {
-
-        model.addAttribute("surveyList", surveyRepository.findAll());
-
-        return "surveylist"; //surveylist.html
-
-    }
-
-    //Mapping /questionlist endpoint to thymeleaf template questionlist.html
-    @RequestMapping(value = "/questionlist", method = RequestMethod.GET)
-    public String questionList(Model model) {
-
-        model.addAttribute("questionList", questionRepository.findAll());
-
-        return "questionlist"; //questionlist.html
-
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/questions", method = RequestMethod.GET)
-    public @ResponseBody List<Question> questions() {
-
-        return (List<Question>) questionRepository.findAll();
-
-    }
-}
-=======
 package hh.swd22.project.surveyapp.webcontroller;
 
 import hh.swd22.project.surveyapp.domain.Question;
@@ -137,10 +76,11 @@ public class QuestionController {
 
     @CrossOrigin
     @RequestMapping(value = "/questions", method = RequestMethod.GET)
-    public @ResponseBody List<Question> questions() {
+    public @ResponseBody
+    List<Question> questions() {
 
         return (List<Question>) questionRepository.findAll();
 
     }
 }
->>>>>>> a65a3028fe54c51a09f7ee0189a2e29104f05676
+
