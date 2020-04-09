496ec8c68928d73ee2a99245562bc7894fa1b399
Arttu Kesanto <arttu.kesanto@gmail.com>
2020-04-09 09:34:54 +0300

    Added REST functionality.

--- src/main/java/hh/swd22/project/surveyapp/webcontroller/QuestionController.java
+++ src/main/java/hh/swd22/project/surveyapp/webcontroller/QuestionController.java
@@ -8,11 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class QuestionController {
@@ -75,12 +79,33 @@ public class QuestionController {
     //RESTFUL SERVICE ENDPOINTS
 
     @CrossOrigin
-    @RequestMapping(value = "/questions", method = RequestMethod.GET)
+    @RequestMapping(value = "/questions", method = RequestMethod.GET) // CrossOrigin for requests from another service, no parameters needed now - Arttu K, 09.04.2020.
     public @ResponseBody
     List<Question> questions() {
 
         return (List<Question>) questionRepository.findAll();
 
     }
+    
+  //RESTful service to get one product by id.
+  	@CrossOrigin
+  	@RequestMapping(value = "/questions/{id}", method = RequestMethod.GET)
+  	public @ResponseBody Optional <Question> findQuestionRest(@PathVariable Long id) { // Täytyy olla id, ei toimi  tässä! Arttu Kesanto 09.04.2020.
+  		return questionRepository.findById(id); // Laitetaan util-importti optional, jotta voidaan käyttää PathVariablea myös tässä.
+}
+  	
+  	
+    @CrossOrigin
+    @RequestMapping(value = "/questions", method = RequestMethod.POST) // CrossOrigin for requests from another service, no parameters needed now - Arttu K, 09.04.2020.
+    public @ResponseBody Question saveQuestionRest(@RequestBody Question question ) {
+    		return questionRepository.save(question);
+    }
+    
+    // Home page of REST services, Template created with examples
+    @RequestMapping(value="/resthome", method = RequestMethod.GET)
+    public String getRestHome() {	
+    	return "resthome"; // resthome.html, oma tiedosto, ei tule importtien kautta. - Arttu Kesanto 09.04.2020.
+    }
+
 }
 
