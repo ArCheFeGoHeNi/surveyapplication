8d50a759ba058052f8d3383c007e7b07af1ef50d
nikkeasy <nikolas.kunnas@myy.haaga-helia.fi>
2020-05-19 11:55:11 +0300

    Added more test data

--- src/main/java/hh/swd22/project/surveyapp/SurveyappApplication.java
+++ src/main/java/hh/swd22/project/surveyapp/SurveyappApplication.java
@@ -45,6 +45,20 @@ public class SurveyappApplication {
 					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
 			questionRepository.save(new Question("Miksi juot niin usein?", "text",
 					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
+			questionRepository.save(new Question("Onko alkoholi vaikuttanut negatiivisesti opintoihisi?", "text",
+					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
+			questionRepository.save(new Question("Kärsitkö masennuksesta tai muusta vastaavasta psyykkisestä vaivasta alkoholin takia? (Muusta kuin krapulasta)", "text",
+					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
+			questionRepository.save(new Question("Miltä jallu maistuu?", "text",
+					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
+			questionRepository.save(new Question("Juotko olutta?", "text",
+					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
+			questionRepository.save(new Question("Onko opintojen alkaminen lisännyt alkoholin nauttimista?", "text",
+					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
+			questionRepository.save(new Question("Onko darra?", "text",
+					surveyRepository.findBySurveyName("Opiskeluhyvinvointi").get(0)));
+			
+			
 			questionRepository.save(new Question("Öö mee töihin?", "text",
 					surveyRepository.findBySurveyName("Työhyvinvointi").get(0)));
 			questionRepository.save(new Question("Pitäskö mennä töihin senkin yhteiskunnan loiseliö?", "text",
@@ -79,11 +93,108 @@ public class SurveyappApplication {
 					questionRepository.findByQuestionText("Käytkö täysipäiväisesti töissä?").get(0),
 					"Kyllä käyn"));
 			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Käytkö täysipäiväisesti töissä?").get(0),
+					"En käy, KELA maksaa"));
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Käytkö täysipäiväisesti töissä?").get(0),
+					"EN KU OSA-AIKAISESTI XD"));
+			answerRepository.save(new Answer(respondent,
 					questionRepository.findByQuestionText("Montako kertaa viikossa juot alkoholia?").get(0),
 					"Muutaman kerran"));
 			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Montako kertaa viikossa juot alkoholia?").get(0),
+					"Kerran, mutta niin et tuntuu!"));
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Montako kertaa viikossa juot alkoholia?").get(0),
+					"Olen ABSOLUTisti"));
+			answerRepository.save(new Answer(respondent,
 					questionRepository.findByQuestionText("Miksi juot niin usein?").get(0),
 					"En ymmärrä ;_;"));
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Miksi juot niin usein?").get(0),
+					"Koska on JANO"));
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Miksi juot niin usein?").get(0),
+					"Minäkö?"));
+
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko alkoholi vaikuttanut negatiivisesti opintoihisi?").get(0),
+					"Pari hylättyy tuli mut never stop the madness!"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko alkoholi vaikuttanut negatiivisesti opintoihisi?").get(0),
+					"No siis juu"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko alkoholi vaikuttanut negatiivisesti opintoihisi?").get(0),
+					"Pois se minusta"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Kärsitkö masennuksesta tai muusta vastaavasta psyykkisestä vaivasta alkoholin takia? (Muusta kuin krapulasta)").get(0),
+					"Vähän on darra joo"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Kärsitkö masennuksesta tai muusta vastaavasta psyykkisestä vaivasta alkoholin takia? (Muusta kuin krapulasta)").get(0),
+					"En, koska tasurit on keksitty"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Kärsitkö masennuksesta tai muusta vastaavasta psyykkisestä vaivasta alkoholin takia? (Muusta kuin krapulasta)").get(0),
+					"Send HALP"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Miltä jallu maistuu?").get(0),
+					"O HYVÄÄ NÄÄS"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Miltä jallu maistuu?").get(0),
+					"<3 <3 <3 "));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Miltä jallu maistuu?").get(0),
+					"Ja naurattaahan se"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Juotko olutta?").get(0),
+					"Miksi en joisi olutta?"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Juotko olutta?").get(0),
+					"OKTOBERFEST"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Juotko olutta?").get(0),
+					"Lürps lürps"));
+			
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko opintojen alkaminen lisännyt alkoholin nauttimista?").get(0),
+					"Kyllä vain"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko opintojen alkaminen lisännyt alkoholin nauttimista?").get(0),
+					"Lürps lürps joo kyä näi o"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko opintojen alkaminen lisännyt alkoholin nauttimista?").get(0),
+					"En juo alkoholia :)"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko darra?").get(0),
+					"Ooof"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko darra?").get(0),
+					"No ON"));
+			
+			answerRepository.save(new Answer(respondent,
+					questionRepository.findByQuestionText("Onko darra?").get(0),
+					"ASDJKLASJKLASGLKJGASLKJGASLKJAGS"));
+			
+			
+			
+			
+			
+			
 			
 			//multiRepo.save(new MultiQuestion("LOLLERO."));
 
